82e3257e5fee9aa07c7f1a4b039d3180349a0711
macson777 <macson7777@gmail.com>
2019-05-14 21:44:29 +0300

    index1

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Rabotaet Proga! GO-Go_Go')
+def index1(request):
+    name = request.POST.get('name')
+    return HttpResponse('len is {}'.format(len(name)))
--- src/src/urls.py
+++ src/src/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import index
+from myapp.views import index1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('index1/', index1)
 ]
