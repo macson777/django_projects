c4ebd761d22e6ca97a8f08fd6a457921d9e4486a
macson777 <macson7777@gmail.com>
2019-05-14 22:03:38 +0300

    index2

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index1(request):
-    name = request.POST.get('name')
-    return HttpResponse('len is {}'.format(len(name)))
+# def index1(request):
+#     name = request.POST.get('name')
+#     return HttpResponse('len is {}'.format(len(name)))
+
+def index2(request):
+     age = request.POST.get('age')
+     if int(age) >= 18:
+        return HttpResponse('Hello')
+     else:
+         return HttpResponse('Error! So young!')
--- src/src/urls.py
+++ src/src/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import index1
+# from myapp.views import index1
+from myapp.views import index2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index1/', index1)
+    # path('index1/', index1),
+    path('index2/', index2)
 ]
